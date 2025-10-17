5c5ef1ed3194e5b588d7c2e9177013ed7158e23e
luca-fabbietti <lucafabbietti@yahoo.it>
2025-10-17 15:02:26 +0200

    first commit

---
+++ tests/cup.booking.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('booking', async ({ page }) => {
+  await page.goto('https://mycupmarche.it/prenotazionecittadino/web/guest/searchCf');
+  await page.waitForSelector('#matrice2');
+  await page.waitForSelector('#cf');
+  await page.locator('#matrice2').fill(process.env.NUMERO_RICETTA || '');
+  await page.locator('#cf').fill(process.env.CODICE_FISCALE || '');
+  await page.getByRole('button', { name: 'Ricerca' }).click();
+  await page.waitForSelector('#selectAreaVastaId');
+  await page.locator('#selectAreaVastaId').selectOption('MARCHE');
+  await page.getByRole('button', { name: 'PRENOTA LE PRESTAZIONI' }).click();
+});
